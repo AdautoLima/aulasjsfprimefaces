713222bd85fcff5ac31ed12afb1bcdb401a82688
idealit <idealit@SPWNIDT0002.EGSBR.local>
2018-01-15 17:12:35 -0200

    TRABALHO Modulo: 10 Video 39 - Criando o applicationContext-security.xml do Spring Security - Parte 2

--- src/br/com/project/model/classes/Entidade.java
+++ src/br/com/project/model/classes/Entidade.java
@@ -20,6 +20,8 @@ public class Entidade implements Serializable {
 
 	private String ent_senha;
 
+	private boolean ent_inativo = false;
+	
 	public void setEnt_login(String ent_login) {
 		this.ent_login = ent_login;
 	}
@@ -43,5 +45,15 @@ public class Entidade implements Serializable {
 	public Long getEnt_codigo() {
 		return ent_codigo;
 	}
+
+    public boolean getEnt_inativo() {
+        return ent_inativo;
+    }
+
+    public void setEnt_inativo(boolean ent_inativo) {
+        this.ent_inativo = ent_inativo;
+    }
+	
+	
 
 }
